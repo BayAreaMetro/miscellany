09036a0541ab45d89cc7fffefd60c8f75a835a8c
Lisa Zorn <lzorn@bayareametro.gov>
2021-10-29 10:32:48 -0700

    Remove total (since it's redundant) and reformat to long for tableau

--- Summarize ACS Mode Share for 2019.R
+++ Summarize ACS Mode Share for 2019.R
@@ -12,7 +12,7 @@ output               <- "M:/Data/Requests/Commissioner Worth/ACS2019"
 censuskey            <- readLines("M:/Data/Census/API/api-key.txt")
 census_api_key(censuskey, install = TRUE, overwrite = TRUE)
 baycounties          <- c("01","13","41","55","75","81","85","95","97")
-ACS_table            <- c(Total_        ="B08006_001",
+ACS_table            <- c(# Total_        ="B08006_001",
                           Drove_Alone_  ="B08006_003",
                           Carpooled_    ="B08006_004",
                           Transit_      ="B08006_008",
@@ -40,11 +40,14 @@ acs_mode <- get_acs(geography = "county", variables = ACS_table,
          Bike         =Bike_E,
          Walk         =Walk_E, 
          Other        =Other_E, 
-         Work_at_Home =Work_at_Home_E, 
-         Total        =Total_E) %>% 
+         Work_at_Home =Work_at_Home_E) %>% 
   arrange(County)
+
+# Convert to long form for Tableau, so columns are County, Commute Mode, Commuters
+acs_mode_long <- pivot_longer(acs_mode, !County, names_to = "Commute Mode", 
+                              values_to = "Commuters")
 
 # Output file
 
-write.csv(acs_mode,file.path(output,"ACS 2019 Means of Transportation by County.csv"),row.names = F)
+write.csv(acs_mode_long,file.path(output,"ACS 2019 Means of Transportation by County.csv"),row.names = F)
 
